c4ce63e4624048284a9c827080ab44d1a32a1e0e
JamesMichaelmiller <127363631+JamesMichaelmiller@users.noreply.github.com>
2023-03-19 18:59:05 -0700

    v2 update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from './components/header/header'
-import HeaderImage from './components/header-image/header-image';
+import HeaderImage from './components/header-image/header-image'
 import Footer from './components/footer/footer';
 function App() {
   return (
     <div>
       <Header />
-      <HeaderImage />
+      <HeaderImage/>
       <Footer />
     </div>
 
